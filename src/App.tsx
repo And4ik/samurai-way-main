4c4b4848a0dfa1f960dd3fcc7e1790ae244b9e3b
And4ik <And4ik11@gmail.com>
2023-03-04 19:06:09 +0100

    Change all all

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,19 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Technologies from "./components/Technologies/Technologies";
-import Accordion from "./components/Accordion/Accordion";
-import {Rating, Star} from "./components/Rating/Rating";
-
+import {Header} from "./components/ Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return (
-        <div>
-{/*            <PageTitle title="This is a App component"/>
-            <PageTitle title="This a App component 2"/>
-            <Header/>
-            <Technologies/>
-            Article 1*/}
-            <Accordion titleValue="Menu" collapsed={true}/>
-            <Accordion titleValue="Users" collapsed={true}/>
-{/*            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>*/}
-
-
-
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
-    )
-        ;
+    );
 }
-type PageTitlePropsType = {
-    title: string
-}
-function PageTitle(props: PageTitlePropsType) {
-    return <h1>{props.title}</h1>
-}
-    export default App;
+
+
+export default App;
 
---
+++ src/components/ Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/1/1c/The_Ascension_Church_of_the_village_karinka.jpg"/>
+        </header>
+    )
+}
